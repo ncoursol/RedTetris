84276306036b29dd17f4eec4e56e5c6ad6fd3d4c
Anroche <antoine-roche@outlook.com>
2024-03-28 11:10:58 +0100

    fix Button

--- front/src/components/Button.vue
+++ front/src/components/Button.vue
@@ -1,40 +1,45 @@
 <template>
-    <button class="actionBtn" @click="$emit('click', { actionType, roomName, username })">
-        <slot>{{ buttonText }}</slot>
-    </button>
+	<button class="actionBtn" @click="emitAction">
+		>
+		<slot>{{ buttonText }}</slot>
+	</button>
 </template>
-  
+
 <script>
 export default {
-    name: "ActionButton",
-    props: {
-        buttonText: String,
-        actionType: String,
-        roomName: String,
-        username: String,
-    },
-}
+	name: 'ActionButton',
+	props: {
+		buttonText: String,
+		actionType: String
+	},
+	methods: {
+		emitAction() {
+			this.$emit('action', this.actionType);
+		}
+	}
+};
 </script>
-  
+
 <style scoped>
 .actionBtn {
-    border: 4px solid black;
-    cursor: pointer;
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    padding: 10px 20px;
-    /* Adjusted padding */
-    letter-spacing: 4px;
-    background-color: transparent;
-    /* Ensure the button is transparent by default */
-    color: inherit;
-    /* Inherit the text color */
-    transition: background-color 0.3s, color 0.3s;
-    /* Smooth transition for hover effect */
+	border: 4px solid black;
+	cursor: pointer;
+	display: inline-flex;
+	align-items: center;
+	justify-content: center;
+	padding: 10px 20px;
+	/* Adjusted padding */
+	letter-spacing: 4px;
+	background-color: transparent;
+	/* Ensure the button is transparent by default */
+	color: inherit;
+	/* Inherit the text color */
+	transition: background-color 0.3s, color 0.3s;
+	/* Smooth transition for hover effect */
 }
 
 .actionBtn:hover {
-    background-color: black;
-    color: white;
-}</style>
+	background-color: black;
+	color: white;
+}
+</style>
